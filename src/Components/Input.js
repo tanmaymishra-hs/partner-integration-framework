4d61384d6033b88af9dae700f2aadca67b2c5012
Tanmay Mishra <tanmaymishra@NOVI-C02G22ADMD6M.local>
2022-10-11 17:12:30 +0530

    Refactor input Component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export default function Input(props) {
+    const [inputValue, setinputValue] = useState("");
+    const handleOnChange = (event)=>{
+        setinputValue(event.target.value)
+    }
     return (
     <>
         {props.type !== "submit" && 
             <label htmlFor={props.name}>{props.name + ": "}</label>
         }
-        <input type={props.type} name={props.name} placeHolder={props.placeHolder}></input><br></br>
+        <input type={props.type} name={props.name} placeHolder={props.placeHolder} value={inputValue} onChange={handleOnChange}></input><br></br>
     </>
     )
 }

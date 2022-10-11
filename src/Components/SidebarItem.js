b9f0e52cac59e4c0f99570d1d6906f59e56c5a3d
Tanmay Mishra <tanmaymishra@NOVI-C02G22ADMD6M.local>
2022-10-11 14:30:47 +0530

    add Sidebar

--- src/Components/SidebarItem.js
+++ src/Components/SidebarItem.js
@@ -1,30 +1,35 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function SidebarItem({item}){
     const [open, setOpen] = useState(false)
 
     
-    if(item.childrens){
+    if(item.children){
         return (
             <div className={open ? "sidebar-item open" : "sidebar-item"}>
-                <div className="sidebar-title">
-                    <span onClick={() => setOpen(!open)}>
+                <div className="sidebar-title" onClick={() => setOpen(!open)}>
+                    <span>
                         { item.icon && <i className={item.icon}></i> }
                         {item.title}    
                     </span> 
                     <i className="bi-chevron-down toggle-btn" onClick={() => setOpen(!open)}></i>
                 </div>
                 <div className="sidebar-content">
-                    { item.childrens.map((child, index) => <SidebarItem key={index} item={child} />) }
+                    { item.children.map((child, index) => <SidebarItem key={index} item={child} />) }
                 </div>
             </div>
         )
     }else{
         return (
-            <a href={item.path} className="sidebar-item plain">
-                { item.icon && <i className={item.icon}></i> }
-                {item.title}
-            </a>
+            <Link to={item.path} className="sidebar-item plain">
+              { item.icon && <i className={item.icon}></i> }
+              {item.title}
+            </Link>
+            // {/* <a href={item.path} className="sidebar-item plain">
+            //     { item.icon && <i className={item.icon}></i> }
+            //     {item.title}
+            // </a> */}
         )
     }
 }

40d65f0560c2fdce12b02f5619d67016e0e8e00f
Tanmay Mishra <tanmaymishra@NOVI-C02G22ADMD6M.local>
2022-10-12 14:47:59 +0530

    Fixed Input.js component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -6,15 +6,15 @@ export default function Input(props) {
         setinputValue(event.target.value)
     }
     return (
-    <>
+    <div >
         {props.inputType !== "submit" && 
             <label style={{'display':'inline-block'}} htmlFor={props.name}>
                 <p style={{'display':'block'}}>{props.name + ": "}</p>
-                <p style={{'display':'block'}}>{props.type}</p>
+                <i style={{'display':'block', 'color':'#808080'}}>{'('+props.type+')'}</i>
                 <p style={{'display':'block'}}>{props.requestContentType}</p>
             </label>
         }
-        <input style={{'display':'inline-block'}} type={props.inputType} name={props.name} placeHolder={props.placeHolder} value={inputValue} onChange={handleOnChange}></input><br></br>
-    </>
+        <input style={{'display':'inline-block', 'position':'absolute'}} type={props.inputType} name={props.name} placeHolder={props.placeHolder} value={inputValue} onChange={handleOnChange}></input><br></br>
+    </div>
     )
 }

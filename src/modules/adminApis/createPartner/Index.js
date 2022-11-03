3360e043244aa15bf9857854e1f107b127cb9629
Swapnil Kasliwal <swapnil.kasliwal.int@hotstar.com>
2022-11-03 14:49:13 +0530

    iauth integration and addition of logic

--- src/modules/adminApis/createPartner/Index.js
+++ src/modules/adminApis/createPartner/Index.js
@@ -33,8 +33,17 @@ export function Index() {
             }
             }
             catch(error)
-            {
-              setResult("Error Case: Your API request did not. The status code received was "+response.status+" with message "+response.statusText)
+            { 
+              if (response.status === 401) {
+                const sign_in_url = response.headers.get('X-HS-IAuth-Redirect-SignIn');
+                if (sign_in_url) {
+                  window.location.href = sign_in_url;
+                }
+              }
+              else{
+                setResult("Error Case: Your API request did not. The status code received was "+response.status+" with message "+response.statusText)
+              }
+              
             }
         }          
         catch(error){console.error(error)}
--- src/modules/adminApis/listPartners/Index.js
+++ src/modules/adminApis/listPartners/Index.js
@@ -32,8 +32,16 @@ export function Index() {
               }
               catch(error)
               {
+                if (response.status === 401) {
+                  const sign_in_url = response.headers.get('X-HS-IAuth-Redirect-SignIn');
+                  if (sign_in_url) {
+                    window.location.href = sign_in_url;
+                  }
+                }
+                else{
                 setResult("Error Case: Your API request did not. The status code received was "+response.status+" with message "+response.statusText)
               }
+            }
             }
     
         catch(error){console.error(error)}

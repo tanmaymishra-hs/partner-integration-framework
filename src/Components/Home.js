ce0e3553398f79e902baf2c17f2bad5fda6338da
Swapnil Kasliwal <swapnil.kasliwal.int@hotstar.com>
2022-11-03 17:30:17 +0530

    change in message

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import Title from './Title'
 export default function Home() {
   return (
     <div>
-      <Title value={"Welcome to Partner Oboarding Portal"}></Title> 
+      <Title value={"Welcome to Partner Onboarding Portal"}></Title> 
       <p>{"Here you can test several apis provided by Hotstar"}</p>
     </div>
   )

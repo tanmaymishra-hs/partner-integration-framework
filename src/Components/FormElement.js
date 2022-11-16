62db08c61fcfe705c93934290a86bfc06eadfc77
Tanmay Mishra <tanmaymishra@NOVI-C02G22ADMD6M.local>
2022-11-16 16:42:53 +0530

    Remove br tag comments

--- src/Components/FormElement.js
+++ src/Components/FormElement.js
@@ -26,41 +26,30 @@ export default function FormElement(props) {
     <CardContent className='boxClass' style={{"padding":"50px"}}>
     <form>
         <h2>{title}</h2>
-        {/* <br/> */}
         <h3 className='Body-1-Medium'>{description}</h3>
-        {/* <br/> */}
-        {/* <br/> */}
         <div className='Input'>
         {pathParams && pathParams.map((element, idx) => (
           <>
           <Input values = {values} setValues = {setValues} name = {element.name} inputType = {element.type} placeholder = {element.name} type={element.type} requestContentType={element.requestContentType}/>
-            {/* <br/> */}
           </>
         ))}
         {queryParams && queryParams.map((element, idx) => (
           <>
         <Input values = {values} setValues = {setValues} name = {element.name} inputType = {element.type} placeholder = {element.name} type={element.type} requestContentType={element.requestContentType}/>
-            {/* <br/> */}
           </>
         ))}
         {headers && headers.map((element, idx) => (
           <>
             <Input values = {values} setValues = {setValues} name = {element.name} inputType = {element.type} placeholder = {element.name} type={element.type} requestContentType={element.requestContentType}/>
-            {/* <br/> */}
             </>
         ))}
         {bodyParams && bodyParams.map((element, idx) => (
           <>
             <Input values = {values} setValues = {setValues} name = {"RequestBody"} inputType = {"JSON"} placeholder = {element.RequestBody} type={element.type} requestContentType={element.requestContentType}/>
-            {/* <br/> */}
             </>
         ))}
         {
           bodyParams&& <>
-          {/* <br/>
-          <br/>
-          <br/>
-          <br/> */}
           </> 
         }
         {!isLoading && <button className="btn ON_BRAND BUTTON1_SEMIBOLD" style={{"width":"100%"}} input="submit" onClick={handleSubmitLocal}>Execute</button>}

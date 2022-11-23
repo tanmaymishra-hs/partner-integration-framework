3c0e1c95c87be8e3474e199a2f23a2403f29b456
Swapnil Kasliwal <swapnil.kasliwal.int@hotstar.com>
2022-11-23 16:30:32 +0530

    fixing the bug in using same module for all apis

--- src/App.js
+++ src/App.js
@@ -35,19 +35,19 @@ function App() {
       <Sidebar className="sidebarClass"/>
       <div className="container">
         <Routes>
-          <Route exact path="/" element={<Home/>}></Route>
-          <Route exact path="/about" element={<About/>}></Route>
-          <Route exact path="/admin/create-partner" element={<Index config = {config["config"]["apis"]["create"]}/>}></Route>
-          <Route exact path="/admin/delete-partner" element={<Index config = {config["config"]["apis"]["deletePartner"]}/>}></Route>
-          <Route exact path="/admin/get-partner" element={<Index config = {config["config"]["apis"]["getPartner"]}/>}></Route>
-          <Route exact path="/admin/list-partner" element={<Index config = {config["config"]["apis"]["listPartners"]}/>}></Route>
-          <Route exact path="/admin/update-partner" element={<Index config = {config["config"]["apis"]["updatePartner"]}/>}></Route>
-          <Route exact path ="/public/sync/get-subscription" element={<Index config = {config["config"]["apis"]["getSubscription"]}/>}></Route>
+          <Route exact path="/" element={<Home key="Home"/>}></Route>
+          <Route exact path="/about" element={<About key="About"/>}></Route>
+          <Route exact path="/admin/create-partner" element={<Index key="CreatePartner" config = {config["config"]["apis"]["create"]}/>}></Route>
+          <Route exact path="/admin/delete-partner" element={<Index key="DeletePartner" config = {config["config"]["apis"]["deletePartner"]}/>}></Route>
+          <Route exact path="/admin/get-partner" element={<Index key="GetPartner" config = {config["config"]["apis"]["getPartner"]}/>}></Route>
+          <Route exact path="/admin/list-partner" element={<Index key="ListPartner" config = {config["config"]["apis"]["listPartners"]}/>}></Route>
+          <Route exact path="/admin/update-partner" element={<Index key="UpdatePartner" config = {config["config"]["apis"]["updatePartner"]}/>}></Route>
+          <Route exact path ="/public/sync/get-subscription" element={<Index key="GetSubscription" config = {config["config"]["apis"]["getSubscription"]}/>}></Route>
           <Route exact path ="/public/sync/create-subscription" element={<CreateSubscriptionSyncIndex/>}></Route>
           <Route exact path ="/public/sync/create-guest-subscription" element={<CreateGuestSubscriptionSyncIndex/>}></Route>
           <Route exact path ="/public/sync/move-subscription" element={<MoveSubscriptionIndex/>}></Route>
-          <Route exact path ="/public/sync/update-subscription" element={<Index config={config["config"]["apis"]["updateSubscription"]}/>}></Route>
-          <Route exact path ="/public/sync/update-subscription-details" element={<Index config={config["config"]["apis"]["updateSubscriptionDetails"]}/>}></Route>
+          <Route exact path ="/public/sync/update-subscription" element={<Index key="UpdateSubscription" config={config["config"]["apis"]["updateSubscription"]}/>}></Route>
+          <Route exact path ="/public/sync/update-subscription-details" element={<Index key="UpdateSubscriptionDetails" config={config["config"]["apis"]["updateSubscriptionDetails"]}/>}></Route>
           <Route exact path ="/public/sync/update-user-details" element={<UpdateUserDetailsSyncIndex/>}></Route>
           <Route exact path ="/public/async/create-guest-subscription" element={<CreateGuestSubscriptionAsyncIndex/>}></Route>
           <Route exact path ="/public/async/create-subscription" element={<CreateSubscriptionAsyncIndex/>}></Route>

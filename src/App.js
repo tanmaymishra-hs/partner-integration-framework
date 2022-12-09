2ec2a979eb87caba4fb0ebca3d00099decbd8e52
Swapnil Kasliwal <swapnil.kasliwal.int@hotstar.com>
2022-12-09 18:49:58 +0530

    adding doc config and nav link for docs

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {Index as UpdateUserDetailsSyncIndex} from './modules/publicApis/syncApis
 import {Index as CreateGuestSubscriptionAsyncIndex} from './modules/publicApis/asyncApis/createGuestSubscriptionAsync/Index'
 import {Index as CreateSubscriptionAsyncIndex} from './modules/publicApis/asyncApis/createSubscriptionAsync/Index'
 import {Index as UpdateUserDetailsAsyncIndex} from './modules/publicApis/asyncApis/updateUserDetailsAsync/Index'
+import {Documentation} from './Components/Documentation'
 import {  
 Routes,
   Route
@@ -33,23 +34,23 @@ function App() {
           <Route exact path="/" element={<Home key="Home"/>}></Route>
           <Route exact path="/about" element={<About key="About"/>}></Route>
           {/* {Object.keys(config['config']['apis']).map((item, index)=> <Route exact path={config['config']['apis'][item]['route']} element={<Index key={config['config']['apis'][item]['title']} config={config['config']['apis'][item]} iAuthToken={config['config']['apis'][item]['category']==="admin"?iAuthToken:null}/>}></Route>)} */}
-          <Route exact path="/admin/create-partner" element={<Index key="CreatePartner" config = {config["config"]["apis"]["create"]} iAuthToken={config['config']['apis']['create']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path="/admin/delete-partner" element={<Index key="DeletePartner" config = {config["config"]["apis"]["deletePartner"]} iAuthToken={config['config']['apis']['deletePartner']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path="/admin/get-partner" element={<Index key="GetPartner" config = {config["config"]["apis"]["getPartner"]} iAuthToken={config['config']['apis']['getPartner']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path="/admin/list-partner" element={<Index key="ListPartner" config = {config["config"]["apis"]["listPartners"]} iAuthToken={config['config']['apis']['listPartners']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path="/admin/update-partner" element={<Index key="UpdatePartner" config = {config["config"]["apis"]["updatePartner"]} iAuthToken={config['config']['apis']['updatePartner']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path ="/public/sync/get-subscription" element={<Index key="GetSubscription" config = {config["config"]["apis"]["getSubscription"]} iAuthToken={config['config']['apis']['getSubscription']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path ="/public/sync/create-subscription" element={<CreateSubscriptionSyncIndex/>}></Route>
-          <Route exact path ="/public/sync/create-guest-subscription" element={<CreateGuestSubscriptionSyncIndex/>}></Route>
-          <Route exact path ="/public/sync/move-subscription" element={<MoveSubscriptionIndex/>}></Route>
-          <Route exact path ="/public/sync/update-subscription" element={<Index key="UpdateSubscription" config={config["config"]["apis"]["updateSubscription"]} iAuthToken={config['config']['apis']['updateSubscription']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path ="/public/sync/update-subscription-details" element={<Index key="UpdateSubscriptionDetails" config={config["config"]["apis"]["updateSubscriptionDetails"]} iAuthToken={config['config']['apis']['updateSubscriptionDetails']['category']==="admin"?iAuthToken:null}/>}></Route>
-          <Route exact path ="/public/sync/update-user-details" element={<UpdateUserDetailsSyncIndex/>}></Route>
-          <Route exact path ="/public/async/create-guest-subscription" element={<CreateGuestSubscriptionAsyncIndex/>}></Route>
-          <Route exact path ="/public/async/create-subscription" element={<CreateSubscriptionAsyncIndex/>}></Route>
-          <Route exact path ="/public/async/update-user-details" element={<UpdateUserDetailsAsyncIndex/>}></Route>
-        
-        
+          <Route exact path="/admin/create-partner/test" element={<Index key="CreatePartner" config = {config["config"]["apis"]["create"]} iAuthToken={config['config']['apis']['create']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path="/admin/delete-partner/test" element={<Index key="DeletePartner" config = {config["config"]["apis"]["deletePartner"]} iAuthToken={config['config']['apis']['deletePartner']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path="/admin/get-partner/test" element={<Index key="GetPartner" config = {config["config"]["apis"]["getPartner"]} iAuthToken={config['config']['apis']['getPartner']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path="/admin/list-partner/test" element={<Index key="ListPartner" config = {config["config"]["apis"]["listPartners"]} iAuthToken={config['config']['apis']['listPartners']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path="/admin/update-partner/test" element={<Index key="UpdatePartner" config = {config["config"]["apis"]["updatePartner"]} iAuthToken={config['config']['apis']['updatePartner']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path ="/public/sync/get-subscription/test" element={<Index key="GetSubscription" config = {config["config"]["apis"]["getSubscription"]} iAuthToken={config['config']['apis']['getSubscription']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path ="/public/sync/create-subscription/test" element={<CreateSubscriptionSyncIndex/>}></Route>
+          <Route exact path ="/public/sync/create-guest-subscription/test" element={<CreateGuestSubscriptionSyncIndex/>}></Route>
+          <Route exact path ="/public/sync/move-subscription/test" element={<MoveSubscriptionIndex/>}></Route>
+          <Route exact path ="/public/sync/update-subscription/test" element={<Index key="UpdateSubscription" config={config["config"]["apis"]["updateSubscription"]} iAuthToken={config['config']['apis']['updateSubscription']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path ="/public/sync/update-subscription-details/test" element={<Index key="UpdateSubscriptionDetails" config={config["config"]["apis"]["updateSubscriptionDetails"]} iAuthToken={config['config']['apis']['updateSubscriptionDetails']['category']==="admin"?iAuthToken:null}/>}></Route>
+          <Route exact path ="/public/sync/update-user-details/test" element={<UpdateUserDetailsSyncIndex/>}></Route>
+          <Route exact path ="/public/async/create-guest-subscription/test" element={<CreateGuestSubscriptionAsyncIndex/>}></Route>
+          <Route exact path ="/public/async/create-subscription/test" element={<CreateSubscriptionAsyncIndex/>}></Route>
+          <Route exact path ="/public/async/update-user-details/test" element={<UpdateUserDetailsAsyncIndex/>}></Route>
+          <Route exact path ="/public/sync/create-subscription/doc" element={<Documentation></Documentation>}></Route>
+  
         </Routes>}
       </div> 
       </ErrorBoundary>
---
+++ src/Components/Documentation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Documentation() {
+  return (
+    <div>
+      <h1>This will be used for  documentation</h1>
+    </div>
+  )
+}

5cfbbb5e6c835341bed9256fe045e9a278798c5c
Tanmay Mishra <tanmaymishra@NOVI-C02G22ADMD6M.local>
2022-12-12 16:36:05 +0530

    Add Snippet and Conetent type documentation

--- src/Components/Documentation.js
+++ src/Components/Documentation.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import configDoc from '../docs/CreateSubsSync.json';
+import ContentDoc from './ContentDoc';
 export function Documentation() {
+  const contentArr = configDoc.filter((item)=>item['type']==='content')
+  const tableArr = configDoc.filter((item)=>item['type']==='table')
+  const snippetArr = configDoc.filter((item)=>item['type']==='snippet')
+  contentArr.map((item, index)=>{
+    console.log(item['title'])
+    // {item['description'].map((t, index)=>console.log(t))}
+  })
   return (
     <div>
-      <h1>This will be used for  documentation</h1>
+      <h1>{configDoc[0]['title']}</h1>
+      {configDoc[0]['description'].map((item, index) => <p>{item}</p>)}
+      <ContentDoc contentArr={contentArr}></ContentDoc>
+      <ContentDoc contentArr={snippetArr}></ContentDoc>
+      {/* {tableArr.map((item, index)=><table>
+  <tr>
+    {item['Cols'].map((c, x)=><th>{c}</th>)}
+  </tr>
+  <tr>
+    {item['Rows'].map((r, x)=><td>{r}</td>)}
+  </tr>
+</table>)} */}
     </div>
   )
 }
